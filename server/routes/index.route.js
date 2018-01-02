058ac364e2d492b04434f3ce83a08feccb25524b
Andrés Reales <andryr2_94@hotmail.com>
2018-01-02 10:50:29 -0600

    cleaning up

--- server/routes/index.route.js
+++ server/routes/index.route.js
@@ -1,7 +1,5 @@
 import express from 'express';
-import userRoutes from './user.route';
 import authRoutes from './auth.route';
-import postRoutes from './post.route';
 import contactRoutes from './contact.route';
 
 const router = express.Router(); // eslint-disable-line new-cap
@@ -11,13 +9,9 @@ router.get('/health-check', (req, res) =>
   res.send('OK')
 );
 
-// mount user routes at /users
-router.use('/users', userRoutes);
-
 // mount auth routes at /auth
 router.use('/auth', authRoutes);
 
-router.use('/posts', postRoutes);
 router.use('/contact', contactRoutes);
 
 export default router;
