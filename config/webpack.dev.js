740295181666e3106b6af9687ba25dcfd7b2107b
Andrés Reales <andryr2_94@hotmail.com>
2018-01-05 10:23:33 -0600

    changed from localhost to 0.0.0.0

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -241,8 +241,8 @@ module.exports = function (options) {
         // });
       },
       proxy: {
-        '/api':  `http://localhost:${process.env.SERVER_PORT || '4040'}`
-        // '/api':  `http://0.0.0.0:${process.env.SERVER_PORT || '4040'}`
+        // '/api':  `http://localhost:${process.env.SERVER_PORT || '4040'}`
+        '/api':  `http://0.0.0.0:${process.env.SERVER_PORT || '4040'}`
       }
     },
 
